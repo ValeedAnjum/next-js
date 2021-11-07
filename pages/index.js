146438b04bc254de538eb220ec552d18d8c40c2b
ValeedAnjum <allinone644@gmail.com>
2021-11-07 18:59:18 +0500

    navbar and footer added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../comps/Footer";
+import Navbar from "../comps/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>HomePage</h1>
       <p>
         Nulla anim est laboris reprehenderit culpa. Dolor elit fugiat do do anim
@@ -14,14 +17,7 @@ export default function Home() {
         nostrud consectetur minim. Veniam et ullamco dolore veniam Lorem ullamco
         elit tempor. Incididunt ullamco ad quis eu.
       </p>
-      <p>
-        Nulla anim est laboris reprehenderit culpa. Dolor elit fugiat do do anim
-        commodo eu officia Lorem occaecat do eu amet ea. Ipsum amet consequat
-        culpa tempor exercitation laborum nisi do id. Cupidatat officia aliquip
-        officia sunt. Laboris ut reprehenderit cupidatat et mollit id esse
-        nostrud consectetur minim. Veniam et ullamco dolore veniam Lorem ullamco
-        elit tempor. Incididunt ullamco ad quis eu.
-      </p>
+      <Footer />
     </div>
   );
 }
