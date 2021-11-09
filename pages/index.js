3176f8f5f32e4a225f92ebd8594a441c4dbc5d76
ValeedAnjum <allinone644@gmail.com>
2021-11-09 23:07:47 +0500

    Linking between paegs

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import Footer from "../comps/Footer";
 import Navbar from "../comps/Navbar";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
